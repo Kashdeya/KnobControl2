d7f745db32f08d28cb3f5f14cbd405e4ea782c68
Tom Wright <tomawright95@outlook.com>
2017-08-16 11:20:18 +0100

    Tweaked BedrockHandler.java

--- src/main/java/com/kashdeya/knobcontrol/handlers/BedrockHandler.java
+++ src/main/java/com/kashdeya/knobcontrol/handlers/BedrockHandler.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
