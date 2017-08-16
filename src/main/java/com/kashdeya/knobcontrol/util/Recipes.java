b760110e2ff0df671d0f4159325adb74d9f7fed1
Tom Wright <tomawright95@outlook.com>
2017-08-16 11:29:35 +0100

    Tweaked Recipes.java to include ability to remove recipes

--- src/main/java/com/kashdeya/knobcontrol/util/Recipes.java
+++ src/main/java/com/kashdeya/knobcontrol/util/Recipes.java
@@ -1,16 +1,23 @@
 package com.kashdeya.knobcontrol.util;
 
 import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class Recipes
 {
+    public static Set<ResourceLocation> removeList = Sets.newHashSet();
+    
     public static void addShapedRecipe(ItemStack output, Object... inputs)
     {
         GameRegistry.addShapedRecipe(output.getItem().getRegistryName(), output.getItem().getRegistryName(), output, inputs);
@@ -53,5 +60,14 @@ public class Recipes
         }
         
         GameRegistry.addShapelessRecipe(output.getItem().getRegistryName(), output.getItem().getRegistryName(), output, ingredients);
+    }
+    
+    public static void removeRecipe(ItemStack resultItem){
+        CraftingManager.REGISTRY.forEach((recipe) -> {
+            if(ItemStack.areItemsEqual(recipe.getRecipeOutput(), resultItem))
+            {
+                removeList.add(recipe.getRegistryName());
+            }
+        });
     }
 }
