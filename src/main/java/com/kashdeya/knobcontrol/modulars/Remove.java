90d99610b0059c23da0171338ab2c211a7780b78
Tom Wright <tomawright95@outlook.com>
2017-08-16 11:30:32 +0100

    Tweaked Remove.java

--- src/main/java/com/kashdeya/knobcontrol/modulars/Remove.java
+++ src/main/java/com/kashdeya/knobcontrol/modulars/Remove.java
@@ -1,14 +1,11 @@
 package com.kashdeya.knobcontrol.modulars;
 
-import java.util.ArrayList;
-
 import com.kashdeya.knobcontrol.handlers.ModularsHandler;
 import com.kashdeya.knobcontrol.handlers.RemoveHandler;
+import com.kashdeya.knobcontrol.util.Recipes;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.IRecipe;
 
 public class Remove {
 	
@@ -16,75 +13,60 @@ public class Remove {
 		if (ModularsHandler.remove){
 			// Tools
 			if (RemoveHandler.woodenTools){
-				removeRecipe(new ItemStack(Items.WOODEN_AXE));
-				removeRecipe(new ItemStack(Items.WOODEN_HOE));
-				removeRecipe(new ItemStack(Items.WOODEN_SWORD));
-				removeRecipe(new ItemStack(Items.WOODEN_SHOVEL));
-				removeRecipe(new ItemStack(Items.WOODEN_PICKAXE));	
+				Recipes.removeRecipe(new ItemStack(Items.WOODEN_AXE));
+				Recipes.removeRecipe(new ItemStack(Items.WOODEN_HOE));
+				Recipes.removeRecipe(new ItemStack(Items.WOODEN_SWORD));
+				Recipes.removeRecipe(new ItemStack(Items.WOODEN_SHOVEL));
+				Recipes.removeRecipe(new ItemStack(Items.WOODEN_PICKAXE));	
 			}
 			if (RemoveHandler.stoneTools){
-				removeRecipe(new ItemStack(Items.STONE_AXE));
-				removeRecipe(new ItemStack(Items.STONE_HOE));
-				removeRecipe(new ItemStack(Items.STONE_SWORD));
-				removeRecipe(new ItemStack(Items.STONE_SHOVEL));
-				removeRecipe(new ItemStack(Items.STONE_PICKAXE));	
+				Recipes.removeRecipe(new ItemStack(Items.STONE_AXE));
+				Recipes.removeRecipe(new ItemStack(Items.STONE_HOE));
+				Recipes.removeRecipe(new ItemStack(Items.STONE_SWORD));
+				Recipes.removeRecipe(new ItemStack(Items.STONE_SHOVEL));
+				Recipes.removeRecipe(new ItemStack(Items.STONE_PICKAXE));	
 			}
 			if (RemoveHandler.ironTools){
-				removeRecipe(new ItemStack(Items.IRON_AXE));
-				removeRecipe(new ItemStack(Items.IRON_HOE));
-				removeRecipe(new ItemStack(Items.IRON_SWORD));
-				removeRecipe(new ItemStack(Items.IRON_SHOVEL));
-				removeRecipe(new ItemStack(Items.IRON_PICKAXE));	
+				Recipes.removeRecipe(new ItemStack(Items.IRON_AXE));
+				Recipes.removeRecipe(new ItemStack(Items.IRON_HOE));
+				Recipes.removeRecipe(new ItemStack(Items.IRON_SWORD));
+				Recipes.removeRecipe(new ItemStack(Items.IRON_SHOVEL));
+				Recipes.removeRecipe(new ItemStack(Items.IRON_PICKAXE));	
 			}
 			if (RemoveHandler.goldenTools){
-				removeRecipe(new ItemStack(Items.GOLDEN_AXE));
-				removeRecipe(new ItemStack(Items.GOLDEN_HOE));
-				removeRecipe(new ItemStack(Items.GOLDEN_SWORD));
-				removeRecipe(new ItemStack(Items.GOLDEN_SHOVEL));
-				removeRecipe(new ItemStack(Items.GOLDEN_PICKAXE));	
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_AXE));
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_HOE));
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_SWORD));
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_SHOVEL));
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_PICKAXE));	
 			}
 			if (RemoveHandler.diamondTools){
-				removeRecipe(new ItemStack(Items.DIAMOND_AXE));
-				removeRecipe(new ItemStack(Items.DIAMOND_HOE));
-				removeRecipe(new ItemStack(Items.DIAMOND_SWORD));
-				removeRecipe(new ItemStack(Items.DIAMOND_SHOVEL));
-				removeRecipe(new ItemStack(Items.DIAMOND_PICKAXE));	
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_AXE));
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_HOE));
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_SWORD));
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_SHOVEL));
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_PICKAXE));	
 			}
 			// Armor
 			if (RemoveHandler.ironArmor){
-				removeRecipe(new ItemStack(Items.IRON_HELMET));
-				removeRecipe(new ItemStack(Items.IRON_CHESTPLATE));
-				removeRecipe(new ItemStack(Items.IRON_LEGGINGS));
-				removeRecipe(new ItemStack(Items.IRON_BOOTS));	
+				Recipes.removeRecipe(new ItemStack(Items.IRON_HELMET));
+				Recipes.removeRecipe(new ItemStack(Items.IRON_CHESTPLATE));
+				Recipes.removeRecipe(new ItemStack(Items.IRON_LEGGINGS));
+				Recipes.removeRecipe(new ItemStack(Items.IRON_BOOTS));	
 			}
 			
 			if (RemoveHandler.goldenArmor){
-				removeRecipe(new ItemStack(Items.GOLDEN_HELMET));
-				removeRecipe(new ItemStack(Items.GOLDEN_CHESTPLATE));
-				removeRecipe(new ItemStack(Items.GOLDEN_LEGGINGS));
-				removeRecipe(new ItemStack(Items.GOLDEN_BOOTS));	
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_HELMET));
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_CHESTPLATE));
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_LEGGINGS));
+				Recipes.removeRecipe(new ItemStack(Items.GOLDEN_BOOTS));	
 			}
 			
 			if (RemoveHandler.diamondArmor){
-				removeRecipe(new ItemStack(Items.DIAMOND_HELMET));
-				removeRecipe(new ItemStack(Items.DIAMOND_CHESTPLATE));
-				removeRecipe(new ItemStack(Items.DIAMOND_LEGGINGS));
-				removeRecipe(new ItemStack(Items.DIAMOND_BOOTS));	
-			}
-		}
-	}
-	
-	private static void removeRecipe(ItemStack resultItem){
-		ItemStack recipeResult;
-		ArrayList recipes = (ArrayList) CraftingManager.getInstance().getRecipeList();
-		for(int scan = 0;scan < recipes.size();scan++){
-			IRecipe tmpRecipe = (IRecipe) recipes.get(scan);
-			recipeResult = tmpRecipe.getRecipeOutput();
-			if(recipeResult != null){
-				if(recipeResult.getItem() == resultItem.getItem() && recipeResult.getItemDamage() == resultItem.getItemDamage()){
-					recipes.remove(scan);
-					scan--;
-				}
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_HELMET));
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_CHESTPLATE));
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_LEGGINGS));
+				Recipes.removeRecipe(new ItemStack(Items.DIAMOND_BOOTS));	
 			}
 		}
 	}
